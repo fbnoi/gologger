affc1aa5a5034b3e616fbca2c80638d34c0e8b8a
fbnoi <fbnoi@foxmail.com>
2023-02-23 23:47:38 +0800

    Update file driver

--- drivers/file.go
+++ drivers/file.go
@@ -9,8 +9,6 @@ import (
 func NewFileDriver(format string) *FileDriver {
 	return &FileDriver{
 		writers: make(map[gologger.Level]*FileWriter),
-		async:   false,
-		wg:      &sync.WaitGroup{},
 		format:  format,
 		mu:      &sync.RWMutex{},
 	}
@@ -18,8 +16,6 @@ func NewFileDriver(format string) *FileDriver {
 
 type FileDriver struct {
 	writers map[gologger.Level]*FileWriter
-	async   bool
-	wg      *sync.WaitGroup
 	format  string
 	mu      *sync.RWMutex
 }
@@ -27,27 +23,16 @@ type FileDriver struct {
 func (fd *FileDriver) AddWriter(level gologger.Level, w *FileWriter) {
 	fd.mu.Lock()
 	defer fd.mu.Unlock()
+
 	fd.writers[level] = w
 }
 
 func (fd *FileDriver) Log(level gologger.Level, log string) {
-	if fd.async {
-		fd.wg.Add(1)
-		go func() {
-			fd.mu.RLock()
-			defer fd.mu.RUnlock()
-			if writer, ok := fd.writers[level]; ok {
-				writer.Write([]byte(log))
-			}
-			fd.wg.Done()
-		}()
-		fd.wg.Wait()
-	} else {
-		fd.mu.RLock()
-		defer fd.mu.RUnlock()
-		if writer, ok := fd.writers[level]; ok {
-			writer.Write([]byte(log))
-		}
+	fd.mu.RLock()
+	defer fd.mu.RUnlock()
+
+	if writer, ok := fd.writers[level]; ok {
+		writer.Write([]byte(log))
 	}
 }
 
